5f567daede93231078559ac761167df1b73e5b65
Will Maclean <macleanwill1@gmail.com>
2025-03-29 23:21:22 +1100

    adding linker script

--- sample_c/build.sh
+++ sample_c/build.sh
@@ -1,3 +1,3 @@
 ../../xpack-riscv-none-elf-gcc-14.2.0-3/bin/riscv-none-elf-gcc  sample_c.c -c -o sample_c.o -nostdlib -march=rv32i
 ../../xpack-riscv-none-elf-gcc-14.2.0-3/bin/riscv-none-elf-as -o _start.o _start.s -march=rv32i
-../../xpack-riscv-none-elf-gcc-14.2.0-3/bin/riscv-none-elf-ld -o test.elf _start.o sample_c.o
+../../xpack-riscv-none-elf-gcc-14.2.0-3/bin/riscv-none-elf-ld -o test.elf _start.o sample_c.o -T linker_script.ld
---
+++ sample_c/get_starting_addr.py
@@ -0,0 +1,12 @@
+from elftools.elf.elffile import ELFFile
+
+def get_entry_point(elf_file_path):
+    with open(elf_file_path, 'rb') as f:
+        elf = ELFFile(f)
+        entry_point = elf.header['e_entry']
+        print(elf.header)
+        return entry_point
+
+# Example usage
+entry_point = get_entry_point("test.elf")
+print(f"The entry point is at address 0x{entry_point:x}")
