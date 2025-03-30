45c078d129c342eda7f93a61a6d0e80a4caee18c
will-maclean <macleanwill1@gmail.com>
2025-03-30 11:44:52 +1100

    fix sample_c asm

--- sample_c/sample_c.c
+++ sample_c/sample_c.c
@@ -5,12 +5,11 @@ void print_s(char* s, int len){
         char c = s[i];
 
         __asm__ volatile (
-            "addi t0, %1, 0\n"   // Load IO_ADDR into register t0
-            "add t1, t0, %0\n"   // Calculate address: IO_ADDR + i, store in t1
-            "sb %2, 0(t1)"       // Store byte: store c at address calculated in t1
+            "addi t0, %0, 0\n"   // Load IO_ADDR into register t0
+            "sb %1, 0(t0)"       // Store byte: store c at address calculated in t1
             : // No outputs
-            : "r" (i), "r" (IO_ADDR), "r" (c) // Inputs: i (for address offset), IO_ADDR, c (character)
-            : "t0", "t1" // Clobbered registers
+            : "r" (IO_ADDR), "r" (c) // IO_ADDR, c (character)
+            : "t0" // Clobbered registers
         );
     }
 }
