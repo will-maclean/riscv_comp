3b921d24976ebfb4ffb1aa11af4d8778d489a1d0
will-maclean <macleanwill1@gmail.com>
2025-04-07 11:51:36 +1000

    syscall bugfixes

--- src/syscall.cpp
+++ src/syscall.cpp
@@ -1,6 +1,7 @@
 #include "syscall.hpp"
 #include "consts.hpp"
 #include "ram.hpp"
+#include "utils.hpp"
 
 #include <iostream>
 
@@ -12,11 +13,11 @@ enum SYSCALL_CODE {
 void syscall(RAM* ram);
 
 void print_c(char c){
-    std::cout << c;
+    write_to_stdout(std::string(1, c));
 }
 
 void print_d(int d){
-    std::cout << d;
+    write_to_stdout(std::to_string(d));
 }
 
 void syscall(RAM* ram){
@@ -27,15 +28,15 @@ void syscall(RAM* ram){
 
     switch (syscall_code) {
         case SYSCALL_PRINT_C:
-            std::cout << "[SYSCALL] Print char: " << arg1 << std::endl;
+            std::cout << "[SYSCALL] print_c" << std::endl;
             print_c(arg1);
             break;
         case SYSCALL_PRINT_D:
-            std::cout << "[SYSCALL] Print int: " << arg1 << std::endl;
+            std::cout << "[SYSCALL] print_d" << std::endl;
             print_d(arg1);
             break;
         default:
-            std::cerr << "[SYSCALL] Unknown syscall code: " << (int)syscall_code << std::endl;
+            std::cerr << "[SYSCALL] Unknown syscall code: " << syscall_code << std::endl;
             break;
     }
 }
